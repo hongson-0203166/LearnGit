d1ce421db6ee13ea7f50b2f35924fda88d247237
hongson-0203166 <phamhongson1107@gmail.com>
2023-11-08 20:27:46 +0700

    commit the first

---
+++ LearnGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LearnGit
+//
+//  Created by Phạm Hồng Sơn on 08/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
